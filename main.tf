afa3d613f00c6010da8f3eef538218d543a6a42b
amanravi-squareops <aman.ravi@squareops.comm>
2025-03-17 16:37:54 +0530

    enchance the aurora module with latest docker image

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
     Environment = var.environment
   }
   region             = var.region
-  secondary_region   = var.secondary_region != "null" ? var.secondary_region : null # Check if secondary_region is null
+  secondary_region   = var.secondary_region != "null" ? var.secondary_region : null
   role_arn           = var.role_arn
   external_id        = var.external_id
   assume_role_config = length(var.role_arn) > 0 ? { role_arn = var.role_arn } : null
@@ -36,7 +36,7 @@ module "aurora" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "8.3.0"
   name    = format("%s-%s", var.environment, var.rds_instance_name)
-  # region                 = var.region
+
   engine                 = var.engine
   engine_mode            = var.engine_mode
   engine_version         = var.engine_mode == "serverless" ? null : var.engine_version
@@ -63,7 +63,7 @@ module "aurora" {
   # security_groups = var.allowed_security_groups
   security_group_rules = {
     ingress_postgresql = {
-      description = "Allow inbound traffic from trusted CIDR blocks"
+      description = "Allow inbound PostgreSQL traffic from trusted CIDR blocks"
       type        = "ingress"
       from_port   = var.port
       to_port     = var.port
@@ -78,47 +78,48 @@ module "aurora" {
       protocol    = "-1"
       cidr_blocks = ["0.0.0.0/0"]
     }
-    subnets         = var.subnets
-    master_password = var.master_password != "" ? var.master_password : (length(random_password.master) > 0 ? random_password.master[0].result : null)
-
-
-    deletion_protection         = var.deletion_protection
-    allow_major_version_upgrade = var.allow_major_version_upgrade
-    skip_final_snapshot         = var.skip_final_snapshot
-    # final_snapshot_identifier_prefix   = var.final_snapshot_identifier_prefix
-    snapshot_identifier                = var.snapshot_identifier
-    backup_retention_period            = var.backup_retention_period
-    preferred_maintenance_window       = var.preferred_maintenance_window
-    preferred_backup_window            = var.preferred_backup_window
-    apply_immediately                  = var.apply_immediately
-    db_parameter_group_name            = aws_db_parameter_group.rds_parameter_group.id
-    db_cluster_parameter_group_name    = aws_rds_cluster_parameter_group.rds_cluster_parameter_group.id
-    serverlessv2_scaling_configuration = var.serverlessv2_scaling_configuration
-    autoscaling_enabled                = var.autoscaling_enabled
-    autoscaling_max_capacity           = var.autoscaling_max
-    autoscaling_min_capacity           = var.autoscaling_min
-    autoscaling_target_cpu             = var.autoscaling_cpu
-    autoscaling_target_connections     = var.autoscaling_target_connections
-    autoscaling_scale_in_cooldown      = var.autoscaling_scale_in_cooldown
-    autoscaling_scale_out_cooldown     = var.autoscaling_scale_out_cooldown
-    predefined_metric_type             = var.predefined_metric_type
-
-    performance_insights_enabled          = var.performance_insights_enabled
-    performance_insights_kms_key_id       = var.performance_insights_kms_key_id
-    performance_insights_retention_period = var.performance_insights_retention_period
-    iam_database_authentication_enabled   = var.iam_database_authentication_enabled
-
-    create_monitoring_role          = var.create_monitoring_role
-    iam_role_name                   = format("%s-%s-%s", var.environment, var.rds_instance_name, "monitoring-role")
-    monitoring_interval             = var.monitoring_interval
-    security_group_description      = var.security_group_description
-    enabled_cloudwatch_logs_exports = var.engine_mode == "provisioned" ? (var.engine == "aurora-mysql" ? ["audit", "error", "general", "slowquery"] : ["postgresql"]) : null
-    tags = merge(
-      { "Name" = format("%s-%s", var.environment, var.rds_instance_name) },
-      local.tags,
-    )
   }
+  subnets         = var.subnets
+  master_password = var.master_password != "" ? var.master_password : (length(random_password.master) > 0 ? random_password.master[0].result : null)
+
+
+  deletion_protection         = var.deletion_protection
+  allow_major_version_upgrade = var.allow_major_version_upgrade
+  skip_final_snapshot         = var.skip_final_snapshot
+  # final_snapshot_identifier_prefix   = var.final_snapshot_identifier_prefix
+  snapshot_identifier                = var.snapshot_identifier
+  backup_retention_period            = var.backup_retention_period
+  preferred_maintenance_window       = var.preferred_maintenance_window
+  preferred_backup_window            = var.preferred_backup_window
+  apply_immediately                  = var.apply_immediately
+  db_parameter_group_name            = aws_db_parameter_group.rds_parameter_group.id
+  db_cluster_parameter_group_name    = aws_rds_cluster_parameter_group.rds_cluster_parameter_group.id
+  serverlessv2_scaling_configuration = var.serverlessv2_scaling_configuration
+  autoscaling_enabled                = var.autoscaling_enabled
+  autoscaling_max_capacity           = var.autoscaling_max
+  autoscaling_min_capacity           = var.autoscaling_min
+  autoscaling_target_cpu             = var.autoscaling_cpu
+  autoscaling_target_connections     = var.autoscaling_target_connections
+  autoscaling_scale_in_cooldown      = var.autoscaling_scale_in_cooldown
+  autoscaling_scale_out_cooldown     = var.autoscaling_scale_out_cooldown
+  predefined_metric_type             = var.predefined_metric_type
+
+  performance_insights_enabled          = var.performance_insights_enabled
+  performance_insights_kms_key_id       = var.performance_insights_kms_key_id
+  performance_insights_retention_period = var.performance_insights_retention_period
+  iam_database_authentication_enabled   = var.iam_database_authentication_enabled
+
+  create_monitoring_role          = var.create_monitoring_role
+  iam_role_name                   = format("%s-%s-%s", var.environment, var.rds_instance_name, "monitoring-role")
+  monitoring_interval             = var.monitoring_interval
+  security_group_description      = var.security_group_description
+  enabled_cloudwatch_logs_exports = var.engine_mode == "provisioned" ? (var.engine == "aurora-mysql" ? ["audit", "error", "general", "slowquery"] : ["postgresql"]) : null
+  tags = merge(
+    { "Name" = format("%s-%s", var.environment, var.rds_instance_name) },
+    local.tags,
+  )
 }
+
 resource "aws_db_parameter_group" "rds_parameter_group" {
   name        = format("%s-%s-parameter-group", var.environment, var.rds_instance_name)
   family      = var.family
@@ -160,7 +161,7 @@ resource "aws_rds_cluster_parameter_group" "rds_cluster_parameter_group" {
 }
 
 resource "aws_secretsmanager_secret" "secret_master_db" {
-  name = format("%s/%s/%s-%s", var.environment, var.rds_instance_name, var.engine, "aurora-password")
+  name = format("%s/%s/%s-%s", var.environment, var.rds_instance_name, var.engine, "aurora-pass")
   tags = merge(
     { "Name" = format("%s/%s/%s-%s", var.environment, var.rds_instance_name, var.engine, "aurora-pass") },
     local.tags,
@@ -195,10 +196,10 @@ resource "aws_rds_global_cluster" "this" {
 }
 
 module "aurora_secondary" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
-  count   = var.global_cluster_enable ? 1 : 0
-  version = "8.3.0"
-  # providers = { aws = aws.secondary }
+  source    = "terraform-aws-modules/rds-aurora/aws"
+  count     = var.global_cluster_enable ? 1 : 0
+  version   = "8.3.0"
+  providers = { aws = aws.secondary }
 
   is_primary_cluster = false
 
@@ -238,7 +239,6 @@ module "aurora_secondary" {
     local.tags,
   )
 }
-
 
 module "backup_restore" {
   depends_on           = [module.aurora]
